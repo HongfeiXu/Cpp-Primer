4fda351aab7a28032187aefbd8b90ff737714b88
Alex <HongfeiXu@users.noreply.github.com>
2016-05-21 10:42:53 +0800

    Update ex3_22.cpp
    I think that is the right answer.

--- ch03/ex3_22.cpp
+++ ch03/ex3_22.cpp
@@ -14,15 +14,21 @@ using std::vector; using std::string; using std::cout; using std::cin; using std
 
 int main()
 {
-    vector<string> text;
-    for (string line; getline(cin, line); text.push_back(line));
-    
-    for (auto& word : text)
-    {
-        for (auto& ch : word)
-            if (isalpha(ch)) ch = toupper(ch);
-        cout << word << " ";
-    }
+	vector<string> text;
+	// 输入每一行
+	string line;
+	while (getline (cin, line))
+		text.push_back (line);
+	// 用迭代器的方式处理
+	for (auto it = text.begin (); it != text.end () && !it->empty(); ++it)		// it为vector<string>::iterator
+
+	{
+		for (auto it2 = it->begin (); it2 != it->end (); ++it2)					// it2为string::iterator
+		{
+			*it2 = toupper (*it2);
+		}
+		cout << *it << endl;
+	}
 
     return 0;
 }
